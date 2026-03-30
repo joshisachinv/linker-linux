aadabde2eb7cbb5b7dafd9c33870ef16d78074c8
Sachin Joshigit <joshisachinv@gmail.com>
2026-03-30 15:04:45 +0200

    updates visual enhancements

--- components/styles.py
+++ components/styles.py
@@ -131,7 +131,8 @@ def apply_custom_css():
         
         /* Targets the individual container for every widget (buttons, toggles, etc) */
         .element-container {
-            margin-bottom: 1rem !important;
+            margin-top: 0.4rem !important;
+            margin-bottom: 0.4rem !important;
         }
 
         /* DIVIDER COMPRESSION */
